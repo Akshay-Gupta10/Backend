2079d52ffcc8437d445bd422e1663c4fd65f4240
Akshay Gupta <akshaygupta@Akshays-MacBook-Air.local>
2026-03-06 13:41:25 +0530

    added

--- downloadS3.js
+++ downloadS3.js
@@ -196,10 +196,7 @@ console.log("upload kr dia")
   })
 });
 
-let result= await response.text();
-
-// console.log("Status Code:", response.status);
-console.log("Message:", result);
+console.log("Status Code:", response.status);
 
 console.log("sb ho gya")
   return {
